392766ccec83e79696cca50ceaa2f58f93b20184
vlhells <vladislav5750@yandex.ru>
2021-11-20 01:22:36 +0300

    -...

--- DLA/Program.cs
+++ DLA/Program.cs
@@ -14,6 +14,7 @@ namespace DLA
             int cluster_V = 1;
             List<Cluster> cluster_parts = new List<Cluster>(cluster_V);
             cluster_parts.Add(new Cluster(field)); // Первый кластер посередине
+            cluster_parts[cluster_V - 1].GetCoord(out int c_x, out int c_y);
 
             int numb_of_globules = 0;
             int globules_i = 0;
@@ -21,19 +22,39 @@ namespace DLA
 
             while (true)
             {
-                if (i > 1 & globules_i == 0)
+                if (i > 1 && globules_i == 0)
                 {
                     numb_of_globules++;
                     globules.Add(new Globule(field));
                     globules_i = 1;
                 }
-                if (globules_i == 1)
+                if (i > 1)
                 {
-                    globules[numb_of_globules - 1].GetCoord(out int x, out int y);
-                    field[x, y] = 0;
-                    globules[numb_of_globules - 1].Move(field);
-                    globules[numb_of_globules - 1].GetCoord(out x, out y);
-                    field[x, y] = 2;
+                    globules[numb_of_globules - 1].GetCoord(out int g_x, out int g_y);
+                    if (globules_i == 1 &&
+                        g_x == c_x + 1 && g_y == c_y + 1 |
+                        g_x == c_x - 1 && g_y == c_y - 1 |
+                        g_x == c_x + 1 && g_y == c_y |
+                        g_x == c_x && g_y == c_y + 1 |
+                        g_x == c_x - 1 && g_y == c_y |
+                        g_x == c_x && g_y == c_y - 1 |
+                        g_x == c_x - 1 && g_y == c_y + 1 |
+                        g_x == c_x + 1 && g_y == c_y - 1)
+                    {
+                        globules.RemoveAt(numb_of_globules - 1);
+                        field[g_x, g_y] = 0;
+                        globules_i = 0;
+                        cluster_V++;
+                        cluster_parts.Add(new Cluster(field));
+                        cluster_parts[cluster_V].GetCoord(out int n_c_x, out int n_c_y);
+                    }
+                    else
+                    {
+                        field[g_x, g_y] = 0;
+                        globules[numb_of_globules - 1].Move(field);
+                        globules[numb_of_globules - 1].GetCoord(out g_x, out g_y);
+                        field[g_x, g_y] = 2;
+                    }
                 }
                 Update();
                 Console.ReadKey();
